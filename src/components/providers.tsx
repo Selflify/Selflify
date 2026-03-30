9646de07deb988899db0232c5f86361ec94dd4f7
aleksnick <nikolaev@iprojects.ru>
2026-03-30 12:21:18 +0300

    fix: remove redundant theme script provider

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { ChakraProvider } from "@chakra-ui/react";
-import { ThemeProvider } from "next-themes";
 
 import { AppToaster } from "@/components/app-toaster";
 import { system } from "@/lib/ui/system";
@@ -12,11 +11,9 @@ type ProvidersProps = {
 
 export function Providers({ children }: ProvidersProps) {
   return (
-    <ThemeProvider attribute="class" forcedTheme="dark">
-      <ChakraProvider value={system}>
-        {children}
-        <AppToaster />
-      </ChakraProvider>
-    </ThemeProvider>
+    <ChakraProvider value={system}>
+      {children}
+      <AppToaster />
+    </ChakraProvider>
   );
 }
